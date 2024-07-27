2bb00da28d97bd3fb1608ccffc638da1ae3db4c9
nulviet <nulvietz@gmail.com>
2024-07-27 19:10:46 +0200

    27-Jul ntb update

--- app.vue
+++ app.vue
@@ -9,7 +9,8 @@
 useHead({
   title: 'Windmill Tree Foundation',
   meta: [
-    { name: 'description', content: 'Altruistic foundation helping people, supporting Ukraine, creating social events and art // something like that :).' }
+    { name: 'description', content: 'Altruistic foundation helping people, supporting Ukraine, creating social events and art // something like that :).' },
+    { name: "google-site-verification", content: "hLKALut4Uu3-zTOQhlRhT-laE6sj4vtjNhZ5UrOyB3s"}
   ]
 })
 
--- pages/Projects/[name].vue
+++ pages/Projects/[name].vue
@@ -49,7 +49,6 @@ onMounted(async () => {
 
 <style lang="scss" scoped>
 .gallery {
-    min-height: 50vh;
     max-height: 100vh;
     padding-top: 2.5vh;
     position: relative;
