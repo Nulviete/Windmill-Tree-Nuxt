759e706ffc44e8b181c1bb4277407643c49322a6
Nulviete <nulvietz@gmail.com>
2024-07-31 17:42:03 +0200

    SEO update for google search

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,16 @@
 export default defineNuxtConfig({
   app: {
     head: {
-      meta: [ { name: "google-site-verification", content: "hLKALut4Uu3-zTOQhlRhT-laE6sj4vtjNhZ5UrOyB3s" }]
+      meta: [ { name: "google-site-verification", content: "hLKALut4Uu3-zTOQhlRhT-laE6sj4vtjNhZ5UrOyB3s" }],
+      script: [{
+        type: 'application/ld+json',
+        innerHTML: JSON.stringify({
+          "@context" : "https://windmilltree.netlify.com",
+          "@type" : "WebSite",
+          "name" : "WindmillTree Foundation",
+          "url" : "https://windmilltree.netlify.com/"
+        })
+      }]
     }
   },
   ssr: false,
