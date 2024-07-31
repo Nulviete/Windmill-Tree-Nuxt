cb21ff43c929d6c787f45300726551db4694bcf6
Nulviete <nulvietz@gmail.com>
2024-07-31 17:19:34 +0200

    modified text on google search

--- app.vue
+++ app.vue
@@ -9,7 +9,7 @@
 useHead({
   title: 'Windmill Tree Foundation',
   meta: [
-    { name: 'description', content: 'Altruistic foundation helping people, supporting Ukraine, creating social events and art // something like that :).' },
+    { name: 'description', content: 'Altruistic foundation helping people, supporting Ukraine, creating social events and art.' },
     { name: "google-site-verification", content: "hLKALut4Uu3-zTOQhlRhT-laE6sj4vtjNhZ5UrOyB3s"}
   ]
 })
