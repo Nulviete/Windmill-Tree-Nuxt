666bbd7b8542aace2ded8b20f3f7d58607f7b346
nulviet <nulvietz@gmail.com>
2025-05-26 11:43:18 +0200

    26-5 favicon link added

--- app.vue
+++ app.vue
@@ -10,6 +10,13 @@ useHead({
   title: 'Windmill Tree Foundation',
   meta: [
     { name: 'description', content: 'Altruistic foundation helping people, supporting Ukraine, creating social events and art.' },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: '/favicon.ico'  // změň podle cesty a typu svého faviconu
+    }
   ]
 })
 
